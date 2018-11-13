0fed003e9bb4e6d2e5f49062153a30d2285ab1e2
lichen <lichen@daojia-inc.com>
2018-11-13 10:36:53 +0800

    add info

---
+++ src/main/java/ink/lichen/mybatisplusdemo/dto/User.java
@@ -0,0 +1,15 @@
+package ink.lichen.mybatisplusdemo.dto;
+
+import lombok.Data;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-13.
+ */
+@Data
+public class User {
+
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+}
---
+++ src/main/java/ink/lichen/mybatisplusdemo/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package ink.lichen.mybatisplusdemo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import ink.lichen.mybatisplusdemo.dto.User;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-13.
+ */
+public interface UserMapper extends BaseMapper<User> {
+}
